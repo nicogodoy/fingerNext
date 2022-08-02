a0c43f745b1ed8e1ffe5bacdd2dc1ea9891d2b28
nico <nicogodoycvu10@gmail.com>
2022-08-02 13:13:50 -0300

    visiId

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,7 +98,7 @@ const Home: NextPage = () => {
   });
   const returnHash = () => {
     if (data) {
-      const hash = md5(JSON.stringify(data, null, 2));
+      const hash = md5(data.visitorId);
       setDataId(hash);
     }
   };
